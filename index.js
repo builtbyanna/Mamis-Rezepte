85828f8e6d4ea8e3214e8cc91ea1dcd05fb549c6
Anna <schusteranna00@gmail.com>
2025-08-05 15:21:39 +0200

    add a tag, empty input

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-let myRecipes = []
-const btn = document.getElementById("save-btn")
-const inputEl = document.getElementById("input-el")
-const ulEl = document.getElementById("ul.el")
+let myRecipes = [];
+const btn = document.getElementById("save-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
 
-btn.addEventListener("click", function save() {
+btn.addEventListener("click", function () {
+myRecipes.push(inputEl.value);
+  renderRecipes()
+  inputEl.value = "";
+});
 
-    myRecipes.push(inputEl.value)
-    ulEl.textContent = myRecipes[i]
-})
-
+function renderRecipes() {
+  let food = "";
+  for (let i = 0; i < myRecipes.length; i++) {
+    food += `<li><a target='_blank' href='${myRecipes[i]}'> ${myRecipes[i]} </a></
+li>`;
+  }
+  ulEl.innerHTML = food;
+}
