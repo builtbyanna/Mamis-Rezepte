1cf290d9b5208d70116fff246b03454d50529267
Anna <schusteranna00@gmail.com>
2025-08-06 11:11:22 +0200

    add localStorage and delete button

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 let myRecipes = [];
 const btn = document.getElementById("save-btn");
+const dbtn = document.getElementById("delete-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const urlRecipes = JSON.parse(localStorage.getItem("myRecipes"))
+
+if (urlRecipes) { //did I find anything saved earlier?
+  myRecipes = urlRecipes //if so put those saved recipes back into myRecipes array
+  renderRecipes() //draws them on the screen
+}
 
 btn.addEventListener("click", function () {
 myRecipes.push(inputEl.value);
+  localStorage.setItem("myRecipes", JSON.stringify(myRecipes))
   renderRecipes()
   inputEl.value = "";
 });
 
+dbtn.addEventListener("dblclick", function() {
+  localStorage.clear()
+  myRecipes = []
+  renderRecipes()
+})
+
 function renderRecipes() {
   let food = "";
   for (let i = 0; i < myRecipes.length; i++) {
-    food += `<li><a target='_blank' href='${myRecipes[i]}'> ${myRecipes[i]} </a></
-li>`;
+    food += `<li>
+    <a target='_blank' href='${myRecipes[i]}'>
+     ${myRecipes[i]} </a>
+     </li>`;
   }
   ulEl.innerHTML = food;
 }
