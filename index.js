aefed7bb9081994ecf2a46de40282f5c9590a2af
Anna <schusteranna00@gmail.com>
2025-08-06 12:43:33 +0200

    add tabBtn

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 let myRecipes = [];
 const btn = document.getElementById("save-btn");
 const dbtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
-const urlRecipes = JSON.parse(localStorage.getItem("myRecipes"))
+const urlRecipes = JSON.parse(localStorage.getItem("myRecipes")); //asks browser if there's anything stored under myRecipes
 
-if (urlRecipes) { //did I find anything saved earlier?
-  myRecipes = urlRecipes //if so put those saved recipes back into myRecipes array
-  renderRecipes() //draws them on the screen
+if (urlRecipes) {
+  //did browser find anything saved earlier?
+  myRecipes = urlRecipes; //if so put's those saved recipes back into myRecipes array
+  render(myRecipes); //draws them on the screen
 }
 
+tabBtn.addEventListener("click", function(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    myRecipes.push(tabs[0].url)
+    localStorage.setItem("myRecipes", JSON.stringify(myRecipes))
+    render(myRecipes)
+  })
+})
+
 btn.addEventListener("click", function () {
-myRecipes.push(inputEl.value);
-  localStorage.setItem("myRecipes", JSON.stringify(myRecipes))
-  renderRecipes()
+  myRecipes.push(inputEl.value);
+  localStorage.setItem("myRecipes", JSON.stringify(myRecipes));
+  render(myRecipes);
   inputEl.value = "";
 });
 
-dbtn.addEventListener("dblclick", function() {
-  localStorage.clear()
-  myRecipes = []
-  renderRecipes()
-})
+dbtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myRecipes = [];
+  render(myRecipes);
+});
 
-function renderRecipes() {
+function render(recipes) {
   let food = "";
-  for (let i = 0; i < myRecipes.length; i++) {
+  for (let i = 0; i < recipes.length; i++) {
     food += `<li>
-    <a target='_blank' href='${myRecipes[i]}'>
-     ${myRecipes[i]} </a>
+    <a target='_blank' href='${recipes[i]}'>
+     ${recipes[i]} </a>
      </li>`;
   }
   ulEl.innerHTML = food;
